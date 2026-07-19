a81a02121fe8f40a5a429ea0a6b920202f04901f
geibyflores75-lgtm <geibyflores75@gmail.com>
2026-07-19 08:54:10 -0600

    Test: Add wildcard route to debug 404.

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Servidor de Prueba Raw Express en línea! 🚀🛡️');
+app.all('*', (req, res) => {
+  res.send(`¡Hola Brahian! El servidor está funcionando y ha recibido una petición a: ${req.url} 🚀🛡️`);
 });
 
 app.listen(port, () => {
