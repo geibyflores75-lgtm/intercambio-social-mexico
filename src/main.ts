13c7059f6bf82490b9203bcd2142a6ef21b23ebe
geibyflores75-lgtm <geibyflores75@gmail.com>
2026-07-19 08:05:52 -0600

    Actualizar main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  
+  // AQUÍ PEGAS EL CÓDIGO
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.send('<html><head><meta name="google-site-verification" content="qa1u4q-stlVHh8E0OD4xZgFutfU_BRVHDpuMJQFNbgk" /></head><body>Intercambio Social Mexico</body></html>');
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
+ 
